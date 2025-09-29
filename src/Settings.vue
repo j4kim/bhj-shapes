be316481f905b18bb302d7083725d78938892574
Joaquim Perez <jivkim@gmail.com>
2025-09-29 23:14:35 +0200

    refactor: new Slider component

--- src/Settings.vue
+++ src/Settings.vue
@@ -1,64 +1,33 @@
 <script setup>
 import { ref } from "vue"
 import { files, settings } from "./tools"
+import Slider from "./Slider.vue"
 
 const open = ref(false)
 </script>
 
 <template>
-    <div class="bg-gray-100">
+    <div class="bg-gray-200/60">
         <div v-if="open" class="flex gap-4">
             <button @click="open = false">←</button>
             <div class="flex gap-2 flex-wrap">
-                <div class="flex flex-col">
-                    <small>Scale min</small>
-                    <input
-                        type="range"
-                        v-model.number="settings.minScale"
-                        min="0"
-                        max="2"
-                        step="0.1"
-                    />
-                </div>
-                <div class="flex flex-col">
-                    <small>Scale max</small>
-                    <input
-                        type="range"
-                        v-model.number="settings.maxScale"
-                        min="0"
-                        max="2"
-                        step="0.1"
-                    />
-                </div>
-                <div class="flex flex-col">
-                    <small>Rotation</small>
-                    <input
-                        type="range"
-                        v-model.number="settings.maxRotation"
-                        min="0"
-                        max="1"
-                        step="0.1"
-                    />
-                </div>
-                <div class="flex flex-col">
-                    <small>Dispersion</small>
-                    <input
-                        type="range"
-                        v-model.number="settings.dispersion"
-                        min="0"
-                        max="1"
-                        step="0.1"
-                    />
-                </div>
-                <div class="flex flex-col">
-                    <small>Shapes</small>
-                    <input
-                        type="range"
-                        v-model.number="settings.take"
-                        min="1"
-                        :max="files.length"
-                    />
-                </div>
+                <Slider
+                    v-model="settings.minScale"
+                    label="Scale min"
+                    :max="2"
+                />
+                <Slider
+                    v-model="settings.maxScale"
+                    label="Scale max"
+                    :max="2"
+                />
+                <Slider v-model="settings.maxRotation" label="Rotation" />
+                <Slider v-model="settings.dispersion" label="Dispersion" />
+                <Slider
+                    v-model="settings.take"
+                    label="Shapes"
+                    :max="files.length"
+                />
             </div>
         </div>
         <button v-else @click="open = true">→</button>
---
+++ src/Slider.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+    label: String,
+    min: {
+        type: Number,
+        default: 0,
+    },
+    max: {
+        type: Number,
+        default: 1,
+    },
+    step: {
+        type: Number,
+        default: 0.1,
+    },
+})
+const model = defineModel({ type: Number })
+</script>
+
+<template>
+    <div class="flex flex-col">
+        <small>{{ label }}</small>
+        <input type="range" v-model.number="model" :min :max :step />
+    </div>
+</template>
