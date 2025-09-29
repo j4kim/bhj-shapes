a9aa879dd5fe4393cc4059c9b696fac23b127d31
Joaquim Perez <jivkim@gmail.com>
2025-09-29 17:56:26 +0200

    install tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import usePHP from "vite-plugin-php";
+import tailwindcss from "@tailwindcss/vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
     base: "",
     plugins: [
         usePHP(),
+        tailwindcss(),
         viteStaticCopy({
             targets: [
                 {
