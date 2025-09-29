09c760b3e4f7911912b89ab94f7cb639ca98061b
Joaquim Perez <jivkim@gmail.com>
2025-09-29 17:42:02 +0200

    use vite-plugin-static-copy instead of public

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from "vite";
 import usePHP from "vite-plugin-php";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-    plugins: [usePHP()],
+    plugins: [
+        usePHP(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "shapes",
+                    dest: ".",
+                },
+            ],
+        }),
+    ],
 });
