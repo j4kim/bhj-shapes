5f2e58df4e7afe6f93c42bb164d41e900eb81b28
Joaquim Perez <jivkim@gmail.com>
2025-09-29 15:49:14 +0200

    feat(app): resize images and keep aspect ratio

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const files = JSON.parse(document.body.dataset.files);
 
+const SIZE = 1000;
+
 const stage = new Konva.Stage({
     container: "container",
-    width: 1000,
-    height: 1000,
+    width: SIZE,
+    height: SIZE,
 });
 
 const layer = new Konva.Layer();
 files.forEach(function (file, i) {
     Konva.Image.fromURL(`shapes/${file}`, function (node) {
-        console.log(file, i);
+        const image = node.image();
+        const ratio = image.width / image.height;
+        const [width, height] =
+            ratio > 1 ? [SIZE, SIZE / ratio] : [SIZE * ratio, SIZE];
+        console.log(file, width, height);
         node.setAttrs({
-            width: 1000,
-            height: 1000,
+            width,
+            height,
             globalCompositeOperation: "multiply",
         });
         layer.add(node);
