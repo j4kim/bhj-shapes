7971bc1220c94794bee16f17676b8dafbbb6a9f8
Joaquim Perez <jivkim@gmail.com>
2025-09-30 17:29:57 +0200

    install tailwind typography and add fake content

--- src/ScrollView.vue
+++ src/ScrollView.vue
@@ -1,3 +1,21 @@
-<script setup></script>
+<script setup>
+import { imageConfigs, windowSize } from "./tools";
+import Content from "./Content.vue";
+</script>
 
-<template>scroll view</template>
+<template>
+    <v-stage
+        :config="{
+            width: windowSize.width.value,
+            height: windowSize.height.value,
+        }"
+    >
+        <v-layer>
+            <v-image
+                v-for="imageConfig in imageConfigs"
+                :config="imageConfig"
+            ></v-image>
+        </v-layer>
+    </v-stage>
+    <Content></Content>
+</template>
