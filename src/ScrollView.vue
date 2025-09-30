f193bf006ad03ef9766ed1e0cf8580a6815d2a59
Joaquim Perez <jivkim@gmail.com>
2025-09-30 17:39:40 +0200

    show content hover canvas

--- src/ScrollView.vue
+++ src/ScrollView.vue
@@ -4,18 +4,20 @@ import Content from "./Content.vue";
 </script>
 
 <template>
-    <v-stage
-        :config="{
-            width: windowSize.width.value,
-            height: windowSize.height.value,
-        }"
-    >
-        <v-layer>
-            <v-image
-                v-for="imageConfig in imageConfigs"
-                :config="imageConfig"
-            ></v-image>
-        </v-layer>
-    </v-stage>
-    <Content></Content>
+    <div class="fixed top-0 z-0">
+        <v-stage
+            :config="{
+                width: windowSize.width.value,
+                height: windowSize.height.value,
+            }"
+        >
+            <v-layer>
+                <v-image
+                    v-for="imageConfig in imageConfigs"
+                    :config="imageConfig"
+                ></v-image>
+            </v-layer>
+        </v-stage>
+    </div>
+    <Content class="relative z-10 bg-white/50 p-4 backdrop-blur-xs"></Content>
 </template>
